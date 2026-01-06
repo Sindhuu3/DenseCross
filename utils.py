eb93030ae2024569ac9d5ac2af35100877a00076
Sindhuja Reddy <161124873+Sindhuu3@users.noreply.github.com>
2026-01-06 15:28:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,25 @@
-import numpy as np #utils.py
-import tensorflow as tf
+import numpy as np
+from PIL import Image
 
-def preprocess_image(image, img_size):
-    img = image.resize(img_size)
-    img_arr = tf.keras.preprocessing.image.img_to_array(img)
-    img_arr = np.expand_dims(img_arr, axis=0)
-    img_arr = img_arr / 255.0
-    return img_arr
+def preprocess_image(image, target_size=(224, 224)):
+    """
+    Preprocess image for DenseNet:
+    - Resize
+    - Normalize
+    - Expand dims
+    """
+
+    if isinstance(image, Image.Image):
+        img = image.resize(target_size)
+        img = np.array(img)
+    else:
+        raise ValueError("Input must be a PIL Image")
+
+    # Normalize
+    img = img.astype("float32") / 255.0
+
+    # Add batch dimension
+    img = np.expand_dims(img, axis=0)
+
+    return img
+
